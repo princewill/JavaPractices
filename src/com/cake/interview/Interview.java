ebb0932607bd3d02560ac490fcbbb4f1d3aa8f72
princewill ohuabunwa <wohuabun@gmail.com>
2019-11-14 21:33:40 -0500

    add fifo

--- src/com/cake/interview/Interview.java
+++ src/com/cake/interview/Interview.java
@@ -8,6 +8,7 @@ import static com.cake.interview.MaxProfit.getMaxProfit;
 import static com.cake.interview.SortScores.sortScores;
 import static com.cake.interview.Meeting.mergeRanges;
 import static com.cake.interview.MergeArrays.mergeArrays;
+import static com.cake.interview.FirstComeFirstServed.isFirstComeFirstServed;
 
 
 
@@ -20,6 +21,7 @@ public class Interview {
         sortScoresExamples();
         mergeRangesSamples();
         mergeArraysSamples();
+        fifoSamples();
     }
 
     private static void bracketValidationExamples() {
@@ -29,7 +31,7 @@ public class Interview {
         System.out.println("Sample 2: ([][]} = " + isValid("([][]}"));
         System.out.println("Sample 3: [[]]()) = " + isValid("[[]]())"));
 
-        System.out.println("End Program____________________________________");
+        System.out.println("End Program____________________________________\n\n");
 
     }
 
@@ -40,7 +42,7 @@ public class Interview {
         System.out.println("Sample 2: {9, 7, 4, 1} = " + getMaxProfit(new int[] {9, 7, 4, 1}));
         System.out.println("Sample 3: {1, 1, 1, 1} = " + getMaxProfit(new int[] {1, 1, 1, 1}));
 
-        System.out.println("End Program____________________________________");
+        System.out.println("End Program____________________________________\n\n");
 
     }
 
@@ -55,7 +57,7 @@ public class Interview {
         System.out.println("Sample 1: unsorted: {37, 89, 41, 65, 91, 53} highestPossibleSCore: 100 ==> " + Arrays.toString(sortScores(scores1, 100)));
         System.out.println("Sample 1: unsorted: {20, 10, 30, 30, 10, 20} highestPossibleSCore: 100 ==> " + Arrays.toString(sortScores(scores2, 100)));
 
-        System.out.println("End Program____________________________________");
+        System.out.println("End Program____________________________________\n\n");
 
     }
 
@@ -75,13 +77,13 @@ public class Interview {
         System.out.println("Sample 1: unsorted: {(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)} = " + (mergeRanges(meetings)));
         System.out.println("Sample 1: unsorted: {(1, 10), (2, 5), (6, 8), (9, 10), (10, 12} = " + (mergeRanges(meetings2)));
 
-        System.out.println("End Program____________________________________");
+        System.out.println("End Program____________________________________\n\n");
 
     }
 
     private static void mergeArraysSamples() {
 
-        System.out.println("Start sort merge ranges Program Examples");
+        System.out.println("Start sort merge arrays Program Examples");
 
         final int[] myArray = {};
         final int[] alicesArray = {};
@@ -95,7 +97,30 @@ public class Interview {
         final int[] alicesArray2 = {1, 3, 7};
         System.out.println("Sample 2: array 1: {2, 4, 6}, array 2: {1, 3, 7} = " + Arrays.toString(mergeArrays(myArray2, alicesArray2)));
 
-        System.out.println("End Program____________________________________");
+        System.out.println("End Program____________________________________\n\n");
+
+    }
+
+    private static void fifoSamples() {
+
+        System.out.println("Start FIFO Program Examples");
+
+        final int[] takeOutOrders = {1, 4, 5};
+        final int[] dineInOrders = {2, 3, 6};
+        final int[] servedOrders = {1, 2, 3, 4, 5, 6};
+        System.out.println("Sample 1: takeOutOrders = {1, 4, 5}, dineInOrders = {2, 3, 6}, servedOrders = {1, 2, 3, 4, 5, 6} = " + isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders));
+
+        final int[] takeOutOrders1 = {1, 5};
+        final int[] dineInOrders1 = {2, 3, 6};
+        final int[] servedOrders1 = {1, 2, 3, 5, 6, 8};
+        System.out.println("Sample 2: takeOutOrders = {1, 5}, dineInOrders = {2, 3, 6}, servedOrders = {1, 2, 3, 5, 6, 8} = " + isFirstComeFirstServed(takeOutOrders1, dineInOrders1, servedOrders1));
+
+        final int[] takeOutOrders2 = {55, 9};
+        final int[] dineInOrders2 = {7, 8};
+        final int[] servedOrders2 = {1, 7, 8, 9};
+        System.out.println("Sample 2: takeOutOrders2 = {55, 9}, dineInOrders2 = {7, 8}, servedOrders2 = {1, 7, 8, 9} = " + isFirstComeFirstServed(takeOutOrders2, dineInOrders2, servedOrders2));
+
+        System.out.println("End Program____________________________________\n\n");
 
     }
 
