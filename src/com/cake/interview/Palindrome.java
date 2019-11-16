6a65d8c468970aed99ce19532459ae03a0f95282
princewill ohuabunwa <wohuabun@gmail.com>
2019-11-16 18:02:08 -0500

    add palindrome

--- src/com/cake/interview/Palindrome.java
+++ src/com/cake/interview/Palindrome.java
@@ -1,4 +1,21 @@
 package com.cake.interview;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Palindrome {
+
+    public static boolean hasPalindromePermutation(String theString) {
+
+        Set<Character> set = new HashSet<>();
+        for (char stringChar : theString.toCharArray()) {
+
+            if (set.contains(stringChar)){
+                set.remove(stringChar);
+            }
+            else set.add(stringChar);
+        }
+
+        return set.size() <= 1;
+    }
 }
