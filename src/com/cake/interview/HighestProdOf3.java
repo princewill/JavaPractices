50f2100756b462c9067f76554bb72e9a2fd04ef4
princewill ohuabunwa <wohuabun@gmail.com>
2019-11-20 16:48:42 -0500

    add highest_product_of_3

---
+++ src/com/cake/interview/HighestProdOf3.java
@@ -0,0 +1,13 @@
+package com.cake.interview;
+
+public class HighestProdOf3 {
+
+    public static int highestProductOf3(int[] intArray) {
+
+        // calculate the highest product of three numbers
+
+
+        return 0;
+    }
+
+}
