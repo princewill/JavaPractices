af83935f971be07d75f2c902d904bf3d124c6765
princewill ohuabunwa <wohuabun@gmail.com>
2019-11-19 19:24:41 -0500

    finalize permutations with specs

--- src/com/cake/interview/Permutations.java
+++ src/com/cake/interview/Permutations.java
@@ -1,5 +1,6 @@
 package com.cake.interview;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,10 +8,29 @@ public class Permutations {
 
     public static Set<String> getPermutations(String inputString) {
 
-        // generate all permutations of the input string
 
+        // base case
+        if (inputString.length() <= 1) {
+            return new HashSet<>(Collections.singletonList(inputString));
+        }
 
-        return new HashSet<>();
+        String allCharsExceptLast = inputString.substring(0, inputString.length() - 1);
+        char lastChar = inputString.charAt(inputString.length() - 1);
+
+        // recursive call: get all possible permutations for all chars except last
+        Set<String> permutationsOfAllCharsExceptLast = getPermutations(allCharsExceptLast);
+
+        // put the last char in all possible positions for each of the above permutations
+        Set<String> permutations = new HashSet<>();
+        for (String permutationOfAllCharsExceptLast : permutationsOfAllCharsExceptLast) {
+            for (int position = 0; position <= allCharsExceptLast.length(); position++) {
+                String permutation = permutationOfAllCharsExceptLast.substring(0, position) + lastChar
+                        + permutationOfAllCharsExceptLast.substring(position);
+                permutations.add(permutation);
+            }
+        }
+
+        return permutations;
     }
 
 
