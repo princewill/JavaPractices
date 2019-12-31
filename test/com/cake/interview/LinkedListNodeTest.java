acd7056d1bfe912451765710cd43f137e9f329b4
princewill ohuabunwa <wohuabun@gmail.com>
2019-12-31 15:14:45 -0500

    add test

--- test/com/cake/interview/LinkedListNodeTest.java
+++ test/com/cake/interview/LinkedListNodeTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static com.cake.interview.LinkedListNode.deleteNodeWhenGivenNode;
+import static com.cake.interview.LinkedListNode.containsCycle;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 
@@ -61,4 +65,68 @@ public class LinkedListNodeTest {
         current.next = new LinkedListNode(value);
         return current.next;
     }
+
+    @Test
+    public void linkedListWithNoCycleTest() {
+        final LinkedListNode[] nodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4});
+        final boolean result = containsCycle(nodes[0]);
+        assertFalse(result);
+    }
+
+    @Test
+    public void cycleLoopsToBeginningTest() {
+        final LinkedListNode[] nodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4});
+        nodes[3].next = nodes[0];
+        final boolean result = containsCycle(nodes[0]);
+        assertTrue(result);
+    }
+
+    @Test
+    public void cycleLoopsToMiddleTest()  {
+        final LinkedListNode[] nodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4, 5});
+        nodes[4].next = nodes[2];
+        final boolean result = containsCycle(nodes[0]);
+        assertTrue(result);
+    }
+
+    @Test
+    public void twoNodeCycleAtEndTest() {
+        final LinkedListNode[] nodes = valuesToLinkedListNodes(new int[] {1, 2, 3, 4, 5});
+        nodes[4].next = nodes[3];
+        final boolean result = containsCycle(nodes[0]);
+        assertTrue(result);
+    }
+
+    @Test
+    public void emptyListTest() {
+        final boolean result = containsCycle(null);
+        assertFalse(result);
+    }
+
+    @Test
+    public void oneElementLinkedListNoCycleTest() {
+        final LinkedListNode node = new LinkedListNode(1);
+        final boolean result = containsCycle(node);
+        assertFalse(result);
+    }
+
+    @Test
+    public void oneElementLinkedListCycleTest() {
+        final LinkedListNode node = new LinkedListNode(1);
+        node.next = node;
+        final boolean result = containsCycle(node);
+        assertTrue(result);
+    }
+
+    private static LinkedListNode[] valuesToLinkedListNodes(int[] values) {
+        final LinkedListNode[] nodes = new LinkedListNode[values.length];
+        for (int i = 0; i < values.length; ++i) {
+            nodes[i] = new LinkedListNode(values[i]);
+            if (i > 0) {
+                nodes [i - 1].next = nodes[i];
+            }
+        }
+        return nodes;
+    }
+
 }
